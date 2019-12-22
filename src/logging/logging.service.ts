0a1e511d765bd7ec71abfe6e0157dbf64aa37720
Stephen McGuckin <donald.stephen.mcguckin@gmail.com>
2019-12-21 20:46:41 -0800

    fixes test

--- src/logging/logging.service.ts
+++ src/logging/logging.service.ts
@@ -1,5 +1,8 @@
 import {LoggingServiceInterface} from './logging.service.interface';
+import {injectable} from 'inversify';
+import 'reflect-metadata';
 
+@injectable()
 export class LoggingService implements LoggingServiceInterface {
     log(...args: any[]): void {
         console.log(...args);
